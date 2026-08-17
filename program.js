d08252f08b8f4e71030fc37c951e329f65105a3f
preciousdev001 <preciouscheng.dev@gmail.com>
2026-08-17 15:19:32 -0700

    finished number 1 on read list

--- program.js
+++ program.js
@@ -4,24 +4,31 @@ const runDatabaseQueries = async () => {
   const db = await connect();
   const movies = db.collection("movies");
 
-  const users = db.collection("users");
+  //   const users = db.collection("users");
 
-  // Run this query, should get top 5 best rated movies on IMDB
-  const topMovies = await movies
-    .find({ "imdb.rating": { $gt: 8.0 } })
-    .project({ title: 1, year: 1, "imdb.rating": 1 })
-    .sort({ "imdb.rating": -1 })
-    .limit(5)
+  //   // Run this query, should get top 5 best rated movies on IMDB
+  //   const topMovies = await movies
+  //     .find({ "imdb.rating": { $gt: 8.0 } })
+  //     .project({ title: 1, year: 1, "imdb.rating": 1 })
+  //     .sort({ "imdb.rating": -1 })
+  //     .limit(5)
+  //     .toArray();
+
+  // added a new user raven baxter into the users collection
+  //   const result = await users.insertOne({
+  //     name: "Raven Baxter",
+  //     email: "raven_baxter@disney.com",
+  //   });
+
+  //   console.log("Top Rated Movies:", topMovies);
+  // show the new user added
+  //   console.log("New user created with ID", result.insertedId);
+
+  //   number 1 find all movies directed by christopher nolan
+  const nolanMovies = await movies
+    .find({ directors: "Christopher Nolan" })
     .toArray();
-
-  const result = await users.insertOne({
-    name: "Raven Baxter",
-    email: "raven_baxter@disney.com",
-  });
-
-  console.log("Top Rated Movies:", topMovies);
-
-  console.log("New user created with ID", result.insertedId);
+  console.log("1. Christopher Nolan Movies:", nolanMovies);
 
   process.exit(0);
 };
