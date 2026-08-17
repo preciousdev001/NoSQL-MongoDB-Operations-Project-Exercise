41603d0a2ffea30fcd3267a2cdd2a782b5afb8a6
preciousdev001 <preciouscheng.dev@gmail.com>
2026-08-17 14:00:59 -0700

    created a new user and connected to mongodb atlas sample mflix db

--- program.js
+++ program.js
@@ -1,23 +1,29 @@
 const connect = require("./db");
 
-
 const runDatabaseQueries = async () => {
-  
   const db = await connect();
-  const movies = db.collection('movies');
+  const movies = db.collection("movies");
 
+  const users = db.collection("users");
 
   // Run this query, should get top 5 best rated movies on IMDB
-  const topMovies = await movies.find({ "imdb.rating": { $gt: 8.0 } })
+  const topMovies = await movies
+    .find({ "imdb.rating": { $gt: 8.0 } })
     .project({ title: 1, year: 1, "imdb.rating": 1 })
     .sort({ "imdb.rating": -1 })
     .limit(5)
     .toArray();
 
-  console.log('Top Rated Movies:', topMovies);
+  const result = await users.insertOne({
+    name: "Raven Baxter",
+    email: "raven_baxter@disney.com",
+  });
+
+  console.log("Top Rated Movies:", topMovies);
+
+  console.log("New user created with ID", result.insertedId);
 
   process.exit(0);
 };
-
 
 runDatabaseQueries();
